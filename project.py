78b72067fea50fa722e8455b553473d58e884507
Hritik <hritikRoshan955@gmail.com>
2023-08-18 18:37:13 +0530

    hindi audio added

--- project.py
+++ project.py
@@ -3,21 +3,31 @@ import win32com.client
 
 speaker =win32com.client.Dispatch("sapi.Spvoice")
 
-def takecommand():
+def takecommand(): # for english
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        query = r.recognize_google(audio, language="en-in")
+        print("your input:", query)
+        return query 
+    
+def takecommand_hindi(): # for hindi part
     r=sr.Recognizer()
     with sr.Microphone() as source:
-        r.pause_threshold=1
-        audio =r.listen(source)
-        query= r.recognize_google(audio, language="en-in" )
-        print("your input: {query}")
+        r.pause_threshold = 1
+        audio=r.listen(source)
+        query=r.recognize_google(audio, language="hi-IN")
+        print("your input: ", query)
+        return query
 
 
 
-while 1:
+while True:
     print("enter the word you want to speak it out by computer")
     s=input()
     speaker.speak(s)
     print("listening...")
-    text= takecommand()
+    text= takecommand_hindi()
     speaker.speak(text)
-
+    
